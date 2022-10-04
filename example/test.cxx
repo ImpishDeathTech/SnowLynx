2e664c09fef7901372db3a2b036555d0e421d15a
Christopher Stephen Rafuse <81781310+ImpishDeathTech@users.noreply.github.com>
2022-10-04 18:37:36 -0400

    Add files via upload

--- example/test.cxx
+++ example/test.cxx
@@ -1,26 +1,6 @@
-#include <SFML/Graphics.hpp>
+#include "include/testlib.hxx"
 
 int main() {
-    sf::RenderWindow win(sf::VideoMode(600, 800), "test", sf::Style::Default);
-    sf::CircleShape  circle(300.f);
-
-    circle.setFillColor(sf::Color::Magenta);
-    circle.setOutlineColor(sf::Color::Yellow);
-    circle.setOutlineThickness(-30);
-
-    while (win.isOpen()) {
-        sf::Event e;
-
-        while (win.pollEvent(e)) {
-            switch (e.type) {
-                case sf::Event::Closed:
-                    win.close();
-                    break;
-            }
-        }
-
-        win.clear();
-        win.draw(circle);
-        win.display();
-    }
+    somefunc();
+    return 0;
 }
