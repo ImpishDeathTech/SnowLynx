b0feb0cce1e529bbe7ec1d54bf3d2749b3676a8c
Christopher Stephen Rafuse <81781310+ImpishDeathTech@users.noreply.github.com>
2022-10-04 20:22:55 +0000

    Update init.lua

--- snowlynx/init.lua
+++ snowlynx/init.lua
@@ -1,3 +1,14 @@
+--
+-- snowlynx/init.lua
+--
+-- BSD 3-Clause License
+--
+-- Copyright (c) 2022, Christopher Stephen Rafuse
+-- All rights reserved.
+--
+-- See LICENSE for details
+--
+
 snow        = require('snowlynx.snowfunc')
 snow.Tab    = require('snowlynx.snowtab')
 snow.Object = require('snowlynx.nclassic')
